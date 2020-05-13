330cbc38d83d7a23404d7b139c2f03b6ec904e48
Tomasz Pietraszko <tomasz.pietraszko@sabre.com>
2020-05-14 01:04:55 +0200

    add router

---
+++ RecipesApp-SPA/src/app/messagess/messagess.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messagess',
+  templateUrl: './messagess.component.html',
+  styleUrls: ['./messagess.component.css']
+})
+export class MessagessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
