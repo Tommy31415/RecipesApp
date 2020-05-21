023ba25ea0a2afa0d97f5ba0fe303ea87ca36d41
Tommy P <tpietraszko@gmail.com>
2020-05-21 08:38:48 +0200

    extend User model; add data seed

--- RecipesApp.API/Data/DataContext.cs
+++ RecipesApp.API/Data/DataContext.cs
@@ -11,5 +11,6 @@ namespace RecipiesApp.API.Data
 
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
--- RecipesApp.API/Migrations/20200521053631_AddUserClass.Designer.cs
+++ RecipesApp.API/Migrations/20200521053631_AddUserClass.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecipiesApp.API.Data;
 
 namespace RecipesApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200521053631_AddUserClass")]
+    partial class AddUserClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
