ce15a4511831db4e7921f4c53e847941fd31b32f
Tommy P <tpietraszko@gmail.com>
2020-05-30 15:02:33 +0200

    add displaying users with details

--- RecipesApp-SPA/src/app/app.module.ts
+++ RecipesApp-SPA/src/app/app.module.ts
@@ -4,8 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery-9';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -18,6 +20,9 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagessComponent } from './messagess/messagess.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 
 export function tokenGetter() {
@@ -33,13 +38,16 @@ export function tokenGetter() {
       MemberListComponent,
       ListsComponent,
       MessagessComponent,
-      MemberCardComponent
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
+      NgxGalleryModule,
+      TabsModule.forRoot(),
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
@@ -52,7 +60,9 @@ export function tokenGetter() {
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      MemberDetailResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
--- RecipesApp-SPA/src/app/routes.ts
+++ RecipesApp-SPA/src/app/routes.ts
@@ -1,20 +1,25 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagessComponent } from './messagess/messagess.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
-    { path: '', component: HomeComponent},
-    { path: '',
+    { path: '', component: HomeComponent },
+    {
+        path: '',
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path: 'members', component: MemberListComponent},
-            { path: 'messages', component: MessagessComponent},
-            { path: 'lists', component: ListsComponent}
+            { path: 'members', component: MemberListComponent , resolve: { users: MemberListResolver}},
+            { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
+            { path: 'messages', component: MessagessComponent },
+            { path: 'lists', component: ListsComponent }
         ]
     },
-    { path: '**', redirectTo: '', pathMatch: 'full'},
+    { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
